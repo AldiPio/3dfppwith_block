5f41f02e3bb643e6fa4d47e29f9a6a664d7f498a
AldiPio <48540821+AldiPio@users.noreply.github.com>
2024-09-13 17:38:17 +0700

    Create box_rumah.py

---
+++ box_rumah.py
@@ -0,0 +1,30 @@
+myBox2 = Entity(model = 'cube',
+               color = color.black,
+               collider = 'box',
+               scale = (0.5, 10, 10), #(lebar , tinggi, panjang)
+               position = (20, 0.5, 5)) #(x , y , z)
+myBox3 = Entity(model = 'cube',
+               color = color.black,
+               collider = 'box',
+               scale = (10, 10, 0.5), #(lebar , tinggi, panjang)
+               position = (15, 0.5, 10)) #(x , y , z)
+myBox4 = Entity(model = 'cube',
+               color = color.black,
+               collider = 'box',
+               scale = (10, 10, 0.5), #(lebar , tinggi, panjang)
+               position = (15, 0.5, 0)) #(x , y , z)
+myBox5 = Entity(model = 'cube',
+               color = color.black,
+               collider = 'box',
+               scale = (0.5, 10, 7), #(lebar , tinggi, panjang)
+               position = (10, 0.5, 6.5)) #(x , y , z)
+myBox6 = Entity(model = 'cube',
+               color = color.green,
+               collider = 'box',
+               scale = (0.5, 3, 3), #(lebar , tinggi, panjang)
+               position = (10, 4, 1.5)) #(x , y , z)
+myBox7 = Entity(model = 'cube',
+               color = color.gold,
+               collider = 'box',
+               scale = (12, 0.5, 11), #(lebar , tinggi, panjang)
+               position = (15, 5.5, 5)) #(x , y , z)
